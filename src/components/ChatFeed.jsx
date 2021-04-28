b2cc62ca1aefe9dd025089a6456b71cef81e8c4e
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-04-29 01:48:21 +0600

    Add function to chat feed

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -7,9 +7,48 @@ import TheirMessage from './TheirMessage';
 const ChatFeed = (props) => {
     const {chats, activeChat, userName, messages} = props;
 
+    const chat = chats && chats[activeChat];
+    
+    const renderMessages = () => {
+        const keys = Object.keys(messages);
+        
+        return keys.map((key, index) => {
+            const message = messages[key];
+            const lastMessageKey = index === 0 ? null: keys[index -1 ];
+            const isMyMessage = userName === message.sender.userName;
+
+            return (
+                <div key= {`msg_${index}`} style= {{width: '100%' }}>
+                    <div className = "message-block">
+                        {
+                            isMyMessage
+                            ? <MyMessage />
+                            : < TheirMessage />
+                        }
+                    </div>
+                    <div className= "read-receipts" style= {{marginRight: isMyMessage ? '180px' : '0px', marginLeft: isMyMessage ? '0px' : '68px' }}>
+                            read-receipts
+                    </div>
+                  </div>
+            )
+        })
+    }
+
+    
+
+    (!chat) return 'Loading.........';
+
     return (
-        <div>
-            ChatFeed
+        <div className="chat-feed">
+            <div className ="chat-title-container">
+    <div className= "chat-title"> {chat.title} </div>
+    <div>
+        {chat.people.map((person) => ` ${person.person.userName}`)}
+           </div>
+
+            </div>
+            {renderMessages()};
+           
         </div>
     )
 
