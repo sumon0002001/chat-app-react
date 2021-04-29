c23bbbcbe4666ae1d42c6a68974c56857069d41c
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-04-29 10:54:35 +0600

    Add function to Mymessage

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,8 +1,18 @@
-const MyMessage = () => {
+const MyMessage = ({ mesaage }) => {
+    if (mesaage?.attachments?.length > 0) {
+        return (
+            <img
+              src= {mesaage.attachments[0].file}
+              alt = "message-attachment"
+              className= "message-imgae"
+              style= {{float: 'right'}}
+              />
+        )
+    }
 
     return (
-        <div>
-            MyMessage
+        <div className="message" style= {{float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A50'}}>
+            {mesaage.texr}
         </div>
     );
 }
