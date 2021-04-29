e6863b02140b4319721d298d896e9b4cff72dbae
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-04-29 19:57:24 +0600

    Add Login function

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import { ChatEngine } from 'react-chat-engine';
-import ChatFeed from './components/ChatFeed';
 
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
-function App() {
-  return (
-     <ChatEngine
-          height = "100vh"
-          projectID = "dfe1eda2-e7be-4eea-b8f5-37143e7c06d9"
-          userName = "sumon0002016"
-          userSecret = "12345678"
-          renderChatFeed = {(chatAppProps) => <ChatFeed {...chatAppProps} />}
-      />
+const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
 
-  
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
   );
-}
+};
+
+// infinite scroll, logout, more customizations...
 
 export default App;
--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,20 +1,20 @@
-const MyMessage = ({ mesaage }) => {
-    if (mesaage?.attachments?.length > 0) {
-        return (
-            <img
-              src= {mesaage.attachments[0].file}
-              alt = "message-attachment"
-              className= "message-imgae"
-              style= {{float: 'right'}}
-              />
-        )
+const MyMessage = ({ message }) => {
+    if (message.attachments && message.attachments.length > 0) {
+      return (
+        <img
+          src={message.attachments[0].file}
+          alt="message-attachment"
+          className="message-image"
+          style={{ float: 'right' }}
+        />
+      );
     }
-
+  
     return (
-        <div className="message" style= {{float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A50'}}>
-            {mesaage.texr}
-        </div>
+      <div className="message" style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A50' }}>
+        {message.text}
+      </div>
     );
-}
-
-export default MyMessage;
+  };
+  
+  export default MyMessage;
